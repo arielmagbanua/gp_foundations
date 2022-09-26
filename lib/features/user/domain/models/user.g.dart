0464d501f7509c020c8933abd92681f24ff70707
unknown <magbanua.c.ariel@gmail.com>
2022-09-26 19:19:07 +0800

    Updated user model.

--- lib/features/user/domain/models/user.g.dart
+++ lib/features/user/domain/models/user.g.dart
@@ -11,6 +11,7 @@ User _$UserFromJson(Map<String, dynamic> json) => User(
       email: json['email'] as String,
       firstName: json['firstName'] as String,
       lastName: json['lastName'] as String,
+      gender: json['gender'] as String,
       verified: json['verified'] as bool? ?? false,
     );
 
@@ -19,5 +20,6 @@ Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'email': instance.email,
       'firstName': instance.firstName,
       'lastName': instance.lastName,
+      'gender': instance.gender,
       'verified': instance.verified,
     };
