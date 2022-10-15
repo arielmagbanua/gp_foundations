85c7af02e6bdee5fa670ca5243db4f853dda1dd2
unknown <magbanua.c.ariel@gmail.com>
2022-10-15 14:02:28 +0800

    Installed get it package and setup the service container.

---
+++ lib/service_container.dart
@@ -0,0 +1,10 @@
+import 'package:get_it/get_it.dart';
+
+import 'features/user/presentaion/cubits/authentication_cubit.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // blocs and cubits
+  sl.registerSingleton(() => AuthenticationCubit());
+}
